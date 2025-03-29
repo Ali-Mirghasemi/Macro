1232fd87af3a5883a69d0d72361e2c9b2a55cb22
Ali <ali.mirghasemi1376@gmail.com>
2025-03-30 00:38:48 +0330

    Add test for MACRO_FOR_TUPLE_ARG

--- Examples/Macro-Test/main.c
+++ Examples/Macro-Test/main.c
@@ -22,23 +22,28 @@ int main()
     int testIndex;
 
     for (testIndex = 0; testIndex < TESTES_LEN; testIndex++) {
-        ASSERT_PRINTF("----------------------------------------------\n\n");
         result = TESTS[testIndex]();
         if (result) {
             errorCount++;
-            ASSERT_PRINTF("Failed\n\n");
+            ASSERT_PRINTF("Failed%s", ASSERT_END_LINE);
+        }
+        else {
+            ASSERT_PRINTF("Pass%s", ASSERT_END_LINE);
         }
     }
+
+    ASSERT_PRINTF("%s", ASSERT_END_LINE);
     if (errorCount > 0) {
-        ASSERT_PRINTF("Error Count: %u\n", errorCount);
+        ASSERT_PRINTF("Error Count: %u%s", errorCount, ASSERT_END_LINE);
     }
     else {
-        ASSERT_PRINTF("Test Pass\n");
+        ASSERT_PRINTF("Test Pass", ASSERT_END_LINE);
     }
 
 }
 
 Assert_Line Test_VA_ARGS(void) {
+    ASSERT_PRINT_HEADER("VA Args", 64, '-');
 
     assertReturnLine(MACRO_VA_ARGS_LEN(), 0);
     assertReturnLine(MACRO_VA_ARGS_LEN(A), 1);
@@ -56,6 +61,7 @@ Assert_Line Test_VA_ARGS(void) {
 }
 
 Assert_Line Test_Str(void) {
+    ASSERT_PRINT_HEADER("Str", 64, '-');
 
     assertReturnLine(Str, MACRO_STR(), "");
     assertReturnLine(Str, MACRO_STR(A), "A");
@@ -68,8 +74,8 @@ Assert_Line Test_Str(void) {
 }
 
 Assert_Line Test_For(void) {
-
-    // -----------------------------------------------
+    ASSERT_PRINT_HEADER("For", 64, '-');
+    // Macro For: Simple array
     #define __MY_FN1(X)     MACRO_STR(X),
 
     const char* ARR_LEFT[] = {
@@ -84,7 +90,7 @@ Assert_Line Test_For(void) {
 
     assertReturnLine(StrList, ARR_LEFT, ARR_RIGHT, 3);
 
-    // -----------------------------------------------
+    // Macro For: String Array and Enum combined
     #define __MY_FN_ENUM(X)         MyEnum_ ##X,
     #define __MY_FN_STR(X)          MACRO_STR(X),
 
@@ -102,7 +108,7 @@ Assert_Line Test_For(void) {
     assertReturnLine(Str, MY_ARR[MyEnum_A1], "A1");
     assertReturnLine(Str, MY_ARR[MyEnum_C1], "C1");
 
-    // -----------------------------------------------
+    // Macro For: Stirng Array & Enum combined with given Argument
     #define __MY_FN_ARG_ENUM(ARG, X)         MyEnum_ ##ARG ##_ ##X,
     #define __MY_FN_ARG_STR(ARG, X)          MACRO_STR(ARG, -, X),
 
@@ -120,7 +126,7 @@ Assert_Line Test_For(void) {
     assertReturnLine(Str, MY_Template_ARR[MyEnum_Template_A1], "Template-A1");
     assertReturnLine(Str, MY_Template_ARR[MyEnum_Template_C1], "Template-C1");
 
-    // -----------------------------------------------
+    // Macro For: Simple Array with multiple arguments for each element
     #define __MY_FN_TUPLE(X, Y)    MACRO_STR(X, <=>, Y),
 
     const char* ARR2_LEFT[] = {
@@ -134,6 +140,30 @@ Assert_Line Test_For(void) {
     };
 
     assertReturnLine(StrList, ARR2_LEFT, ARR2_RIGHT, 3);
-    
+
+    // Macro For: Simple array with multiple arguments for each element and Arguments
+    #define __MY_FN_TUPLE_ARG(ARG, X, Y)    MACRO_STR(ARG, -, X, <=>, Y),
+
+    #define IMPL_ARG_TUPLE_ARRAY(NAME, ...) \
+    const char* TUPLE_ARRAY_ ##NAME [] = { \
+        MACRO_FOR_TUPLE_ARG(__MY_FN_TUPLE_ARG, NAME, __VA_ARGS__) \
+    }
+
+    IMPL_ARG_TUPLE_ARRAY(MY,
+        (A, 4),
+        (X, 2),
+        (V, 6),
+        (Z, 8)
+    );
+
+    const char* TUPLE_ARRAY_RIGHT[] = {
+        "MY-A<=>4",
+        "MY-X<=>2",
+        "MY-V<=>6",
+        "MY-Z<=>8",
+    };
+
+    assertReturnLine(StrList, TUPLE_ARRAY_MY, TUPLE_ARRAY_RIGHT, 4);
+
     return 0;
 }
