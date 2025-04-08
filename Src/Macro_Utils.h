99555f132182fa76c33030e0fc3ce26844043447
Ali Mirghasemi <ali.mirghasemi1376@gmail.com>
2025-04-08 16:43:26 +0330

    Implement Tuple expander

---
+++ Src/Macro_Utils.h
@@ -0,0 +1,12 @@
+#ifndef _MACRO_UTILS_H_
+#define _MACRO_UTILS_H_
+
+
+#define __MACRO_TUPLE_EXPAND__(...)                     __VA_ARGS__
+#define __MACRO_TUPLE_EXPAND_(...)                      __MACRO_TUPLE_EXPAND__(__VA_ARGS__)
+#define __MACRO_TUPLE_EXPAND(X)                         __MACRO_TUPLE_EXPAND_ X
+
+
+
+
+#endif
