9563f4fc07983af32fdb22b8550b46463ab0889e
Ali <ali.mirghasemi1376@gmail.com>
2025-04-11 23:33:53 +0330

    Implement Safe MACRO_ARRAY_LEN

--- Src/Macro_Utils.h
+++ Src/Macro_Utils.h
@@ -1,6 +1,9 @@
 #ifndef _MACRO_UTILS_H_
 #define _MACRO_UTILS_H_
 
+#define __MACRO_ARRAY_LEN__(ARR)                        (sizeof(ARR) / sizeof(ARR[0]))
+#define __MACRO_ARRAY_LEN_(ARR)                         __MACRO_ARRAY_LEN__(ARR)
+#define __MACRO_ARRAY_LEN(ARR)                          __MACRO_ARRAY_LEN_(ARR)
 
 #define __MACRO_TUPLE_EXPAND__(...)                     __VA_ARGS__
 #define __MACRO_TUPLE_EXPAND_(...)                      __MACRO_TUPLE_EXPAND__(__VA_ARGS__)
