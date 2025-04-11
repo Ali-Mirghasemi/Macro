d0671eeedaa542757a3d856dc093136a96653272
Ali <ali.mirghasemi1376@gmail.com>
2025-04-11 22:43:34 +0330

    Move macro fn to Macro_Fn.h

--- Src/Macro_Utils.h
+++ Src/Macro_Utils.h
@@ -6,9 +6,4 @@
 #define __MACRO_TUPLE_EXPAND_(...)                      __MACRO_TUPLE_EXPAND__(__VA_ARGS__)
 #define __MACRO_TUPLE_EXPAND(X)                         __MACRO_TUPLE_EXPAND_ X
 
-#define __MACRO_FN__(FN, ...)                           FN(__VA_ARGS__)
-#define __MACRO_FN_(FN, ...)                            __MACRO_FN__(FN, __VA_ARGS__)
-#define __MACRO_FN(FN, ...)                             __MACRO_FN_(FN, __VA_ARGS__)
-
-
 #endif
