7a87d1e17ee78c9255b7e3460b7d91dbc7eeb08e
Ali <ali.mirghasemi1376@gmail.com>
2025-04-11 23:35:01 +0330

    Implement MACRO_FN_MAP_ARG

--- Src/Macro_Fn.h
+++ Src/Macro_Fn.h
@@ -11,4 +11,12 @@
 #define __MACRO_FN_MAP_(FN_MAP, ...)                    __MACRO_FN_MAP__(FN_MAP, __VA_ARGS__)
 #define __MACRO_FN_MAP(FN_MAP, ...)                     __MACRO_FN_MAP_(FN_MAP, __VA_ARGS__)
 
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+    #define __MACRO_FN_MAP_ARG__(FN_MAP, ARG, ...)      __MACRO_FN(__MACRO_VA_ARGS_MAP(FN_MAP, __VA_ARGS__), ARG __VA_OPT__(,) __VA_ARGS__)
+#else
+    #define __MACRO_FN_MAP_ARG__(FN_MAP, ARG, ...)      __MACRO_FN(__MACRO_VA_ARGS_MAP(FN_MAP, __VA_ARGS__), ARG, ##__VA_ARGS__)
+#endif
+#define __MACRO_FN_MAP_ARG_(FN_MAP, ARG, ...)           __MACRO_FN_MAP_ARG__(FN_MAP, ARG, __VA_ARGS__)
+#define __MACRO_FN_MAP_ARG(FN_MAP, ARG, ...)            __MACRO_FN_MAP_ARG_(FN_MAP, ARG, __VA_ARGS__)
+
 #endif // _MACRO_FN_H_
